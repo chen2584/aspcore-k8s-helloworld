8bcab19132e30f3120f087b75f505ab5e3012d15
Worameth Semapat <worameth.semapat@gmail.com>
2021-01-18 09:51:16 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# api-k8s-helloworld
+
+### Resources
+- 
+
+- https://www.youtube.com/watch?v=ZOROT9yMp4
---
+++ src/MyApi/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
